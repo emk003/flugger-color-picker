45fcbfc43a4adb1f7a871c75dd6afb4a59513c9f
emiliekyllesbech <emilie.restrup@gmail.com>
2025-05-13 16:23:39 +0200

    Lavet en database med farver og forskellige atributes

---
+++ database.js
@@ -0,0 +1,194 @@
+// colors.js
+const colorData = [
+    {
+        id: "FL8005-01",
+        navn: "Light Grey",
+        kode: "FL8005",
+        hex: "#CACDCD",
+        farvekort: "Flügger 80",
+        grundfarve: "Grå",
+        tone: "Lys"
+    },
+    {
+        id: "FL8014-02",
+        navn: "Cold Umbra",
+        kode: "FL8014",
+        hex: "#9C9C95",
+        farvekort: "Flügger 80",
+        grundfarve: "Grå",
+        tone: "Mørk"
+    },
+    {
+        id: "FL8017-03",
+        navn: "Pocket Green",
+        kode: "FL8017",
+        hex: "#D9D9C0",
+        farvekort: "Flügger 80",
+        grundfarve: "Grøn",
+        tone: "Lys"
+    },
+    {
+        id: "FL8024-04",
+        navn: "Bora Mint",
+        kode: "FL8024",
+        hex: "#BDCFC0",
+        farvekort: "Flügger 80",
+        grundfarve: "Grøn",
+        tone: "Lys"
+    },
+    {
+        id: "FL8030-05",
+        navn: "Lund",
+        kode: "FL8030",
+        hex: "#596250",
+        farvekort: "Flügger 80",
+        grundfarve: "Grøn",
+        tone: "Mørk"
+    },
+    {
+        id: "FL8040-06",
+        navn: "Bornholm",
+        kode: "FL8040",
+        hex: "#B9C3C7",
+        farvekort: "Flügger 80",
+        grundfarve: "Blå",
+        tone: "Lys"
+    },
+    {
+        id: "FL8043-07",
+        navn: "Classic Blue",
+        kode: "FL8043",
+        hex: "#6F8C9D",
+        farvekort: "Flügger 80",
+        grundfarve: "Blå",
+        tone: "Kold"
+    },
+    {
+        id: "FL8055-08",
+        navn: "Flamingo",
+        kode: "FL8055",
+        hex: "#D8B8B8",
+        farvekort: "Flügger 80",
+        grundfarve: "Rød",
+        tone: "Kold"
+    },
+    {
+        id: "FL8062-09",
+        navn: "Suede Plum",
+        kode: "FL8062",
+        hex: "#81686B",
+        farvekort: "Flügger 80",
+        grundfarve: "Lilla",
+        tone: "Mørk"
+    },{
+        id: "FL8079-10",
+        navn: "Clay Court",
+        kode: "FL8079",
+        hex: "#BF9F7C",
+        farvekort: "Flügger 80",
+        grundfarve: "Brun",
+        tone: "Varm"
+    },
+    {
+        id: "FL8058-11",
+        navn: "Portofino",
+        kode: "FL8058",
+        hex: "#AA7C78",
+        farvekort: "Flügger 80",
+        grundfarve: "rød",
+        tone: "Mørk"
+    },
+    {
+        id: "FL8077-12",
+        navn: "Alba",
+        kode: "FL8077",
+        hex: "#826B62",
+        farvekort: "Flügger 80",
+        grundfarve: "Brun",
+        tone: "Varm"
+    },
+    {
+        id: "FL8073-13",
+        navn: "Java",
+        kode: "FL8073",
+        hex: "#988E86",
+        farvekort: "Flügger 80",
+        grundfarve: "Grå",
+        tone: "Varm"
+    },
+    {
+        id: "FL8057-14",
+        navn: "Kind Purple",
+        kode: "FL8057",
+        hex: "#978E99",
+        farvekort: "Flügger 80",
+        grundfarve: "Lilla",
+        tone: "Kold"
+    },
+    {
+        id: "FL8068-15",
+        navn: "Pale Yellow",
+        kode: "FL8068",
+        hex: "#EDE7C8",
+        farvekort: "Flügger 80",
+        grundfarve: "Gul",
+        tone: "Kold"
+    },
+    {
+        id: "Kabric-01",
+        navn: "Big Apple",
+        kode: "Kabric",
+        hex: "#818E7F",
+        farvekort: "DETALE Kabric 2025 collection",
+        grundfarve: "Grøn",
+        tone: "Mørk",
+        type: "effekt",
+        tekstur: ""
+    },
+    {
+        id: "Kabric-02",
+        navn: "Ocean Drive",
+        kode: "Kabric",
+        hex: "#B7B8B2",
+        farvekort: "DETALE Kabric 2025 collection",
+        grundfarve: "Grå",
+        tone: "Kold",
+        type: "effekt",
+        tekstur: ""
+    },
+    {
+        id: "Kabric-03",
+        navn: "Warm Teracotta",
+        kode: "Kabric",
+        hex: "#A7765F",
+        farvekort: "DETALE Kabric 2025 collection",
+        grundfarve: "Rød",
+        tone: "Varm",
+        type: "effekt",
+        tekstur: ""
+    },
+    {
+        id: "Kabric-04",
+        navn: "Vintage Blue",
+        kode: "Kabric",
+        hex: "#9EB0AC",
+        farvekort: "DETALE Kabric 2025 collection",
+        grundfarve: "Blå",
+        tone: "Lys",
+        type: "effekt",
+        tekstur: ""
+    },
+    {
+        id: "Kabric-05",
+        navn: "Dessert",
+        kode: "Kabric",
+        hex: "#D2C5AA",
+        farvekort: "DETALE Kabric 2025 collection",
+        grundfarve: "Gul",
+        tone: "Mørk",
+        type: "effekt",
+        tekstur: ""
+    },
+
+
+];
